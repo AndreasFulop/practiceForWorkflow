5e27bc23114d3e37222358c5b5053443d17107e2
AndreasFulop <fulopand@freemail.hu>
2021-08-02 17:45:05 +0200

    new ParabankTests

--- src/test/java/ParabankTests.java
+++ src/test/java/ParabankTests.java
@@ -1,52 +1,114 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.commons.lang3.StringUtils.split;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ParabankTests {
+    private  static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            options.addArguments("incognito");
+            options.addArguments("--disable-gpu","--ignore-certificate-errors","--disable-extensions","--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static String fileReader(String fieldName) {
+        String credentials = "";
+        try {
+            File myObj = new File("paraReg.txt");
+            Scanner scanner = new Scanner(myObj);
+            while(scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                String[] temp = data.split(":");
+                if (temp[0].equals(fieldName)){
+                    credentials = temp[1];
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {e.printStackTrace();}
+        return credentials;
+    }
+
+    public static void closeDriver () {
+        driver.close();
+    }
+
+    public static void clickOnPage(By by) {
+        getDriver().findElement(by).click();
+    }
+
+    public static String getTextFromPage(By by) {
+        return getDriver().findElement(by).getText();
+    }
+
+    public static void sendKeysToField(By by, String string) {
+        getDriver().findElement(by).sendKeys(string);
+    }
+
+    public static void waitForMsec(int time) throws InterruptedException {
+        Thread.sleep(time);
+    }
 
     public static void login() {
-        String username = Util.fileReader("username");
-        String password = Util.fileReader("password");
-        Util.sendKeysToField(MainPage.USERNAME_FIELD, username);
-        Util.sendKeysToField(MainPage.PASSWORD_FIELD, password);
-        Util.clickOnPage(MainPage.LOGIN_BUTTON);
+        String username = fileReader("username");
+        String password = fileReader("password");
+        sendKeysToField(MainPage.USERNAME_FIELD, username);
+        sendKeysToField(MainPage.PASSWORD_FIELD, password);
+        clickOnPage(MainPage.LOGIN_BUTTON);
     }
 
     public static void logout() {
-        Util.clickOnPage(Overview.LOGOUT_MENU);
+        clickOnPage(Overview.LOGOUT_MENU);
     }
 
         @BeforeEach
     public void driverInit() {
-        Util.getDriver().get("https://parabank.parasoft.com/");
+        getDriver().get("https://parabank.parasoft.com/");
     }
 
     @Test
     @Order(1)
     @DisplayName("Main page is valid")
     public void testMainPageSubtitle() {
-        Assertions.assertEquals("Experience the difference", Util.getTextFromPage(MainPage.SUBTITLE));
+        Assertions.assertEquals("Experience the difference", getTextFromPage(MainPage.SUBTITLE));
     }
 
     @Test
     @Order(2)
     @DisplayName("Register page is valid")
     public void testRegister() {
-        Util.clickOnPage(MainPage.REGISTRATION_LINK);
-        Assertions.assertEquals("Signing up is easy!", Util.getTextFromPage(Register.TITLE));
+        clickOnPage(MainPage.REGISTRATION_LINK);
+        Assertions.assertEquals("Signing up is easy!", getTextFromPage(Register.TITLE));
     }
 
     @Test
     @Order(3)
     @DisplayName("About Us page is valid")
     public void testAboutUs() {
-        Util.clickOnPage(MainPage.ABOUT_US);
-        Assertions.assertTrue(Util.getTextFromPage(AboutUs.TEXT_ABOUT).contains("not a real bank"));
+        clickOnPage(MainPage.ABOUT_US);
+        Assertions.assertTrue(getTextFromPage(AboutUs.TEXT_ABOUT).contains("not a real bank"));
     }
 /*
    @Test
@@ -74,21 +136,21 @@ public class ParabankTests {
         @DisplayName("After registrating you can log in")
             public void testLogin() throws InterruptedException {
             login();
-            String actual = Util.getTextFromPage(Overview.TITLE);
+            String actual = getTextFromPage(Overview.TITLE);
             Assertions.assertEquals("Accounts Overview", actual);
             logout();
-            Util.waitForMsec(2000);
-            Assertions.assertEquals("Customer Login", Util.getTextFromPage(MainPage.LOGIN_TITLE));
+            waitForMsec(2000);
+            Assertions.assertEquals("Customer Login", getTextFromPage(MainPage.LOGIN_TITLE));
         }
 
         @Test
         @Order(6)
         @DisplayName("The new account's starting balance")
         public void testBalance() throws InterruptedException {
-            Util.waitForMsec(2000);
+            waitForMsec(2000);
             login();
-            Util.waitForMsec(2000);
-            Assertions.assertEquals("$1692.67", Util.getTextFromPage(Overview.BALANCE));
+            waitForMsec(2000);
+            Assertions.assertEquals("$1692.67", getTextFromPage(Overview.BALANCE));
 
             logout();
         }
@@ -97,30 +159,30 @@ public class ParabankTests {
         @Order(7)
         @DisplayName("Make a new account and find it's number")
         public void testNewAccount() throws InterruptedException {
-            Util.waitForMsec(2000);
+            waitForMsec(2000);
             login();
          //   Util.clickOnPage(Overview.NEW_ACCOUNT_MENU);
          //   Util.clickOnPage(OpenAccount.NEW_ACCOUNT_BUTTON);
-            Util.waitForMsec(2000);
+            waitForMsec(2000);
  //           String newAccountId = Util.getTextFromPage(OpenAccount.NEW_ACCOUNT_ID);
 //            System.out.println(newAccountId);
 
-            Util.clickOnPage(Overview.ACCOUNTS_OVERVIEW_MENU);
+            clickOnPage(Overview.ACCOUNTS_OVERVIEW_MENU);
 
-            String oldAccountId = Util.getTextFromPage(Overview.ACCOUNT_FIRST_NUMBER);
+            String oldAccountId = getTextFromPage(Overview.ACCOUNT_FIRST_NUMBER);
       //      if (newAccountId.equals(oldAccountId)) {
-             String newAccountId = Util.getTextFromPage(Overview.ACCOUNT_SECOND_NUMBER);
+             String newAccountId = getTextFromPage(Overview.ACCOUNT_SECOND_NUMBER);
        //     }
             System.out.println(oldAccountId + ", " + newAccountId);
-            Util.waitForMsec(200);
-            Util.clickOnPage(Overview.TRANSFER_FUNDS_MENU);
-            Util.waitForMsec(2000);
-            Util.sendKeysToField(TransferFunds.AMOUNT_FIELD, "125");
-            Util.clickOnPage(TransferFunds.ACCOUNT_FROM);
-            Util.clickOnPage(TransferFunds.ACCOUNT_FROM_SELECT1);
-            Util.clickOnPage((TransferFunds.ACCOUNT_TO));
-            Util.clickOnPage((TransferFunds.ACCOUNT_TO_SELECT2));
-            Util.waitForMsec(2000);
+            waitForMsec(200);
+            clickOnPage(Overview.TRANSFER_FUNDS_MENU);
+            waitForMsec(2000);
+            sendKeysToField(TransferFunds.AMOUNT_FIELD, "125");
+            clickOnPage(TransferFunds.ACCOUNT_FROM);
+            clickOnPage(TransferFunds.ACCOUNT_FROM_SELECT1);
+            clickOnPage((TransferFunds.ACCOUNT_TO));
+            clickOnPage((TransferFunds.ACCOUNT_TO_SELECT2));
+            waitForMsec(2000);
             logout();
         }
 
@@ -129,8 +191,8 @@ public class ParabankTests {
         @DisplayName("Try to write out the tablecols names")
         public void testTableNames() {
             login();
-            Util.clickOnPage(Overview.ACCOUNTS_OVERVIEW_MENU);
-            String colNames = Util.getTextFromPage(Overview.TABLE_COL_NAMES);
+            clickOnPage(Overview.ACCOUNTS_OVERVIEW_MENU);
+            String colNames = getTextFromPage(Overview.TABLE_COL_NAMES);
             String[] cols = split(colNames);
             for (String i:cols) {
             System.out.println(i);}
@@ -139,6 +201,6 @@ public class ParabankTests {
 
     @AfterAll
     static void collapseAll() {
-    Util.closeDriver();
+    closeDriver();
     }
 }
